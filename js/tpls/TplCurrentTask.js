0bbc0f942599d20bc1b9284ff1238d031ce456b4
Pavel Halkovsky <pavelhalkovsky@gmail.com>
2026-04-23 16:52:00 +0300

    Split TplCurrentTask::init into private functions.

--- js/tpls/TplCurrentTask.js
+++ js/tpls/TplCurrentTask.js
@@ -3,6 +3,12 @@ import Task from '../models/Task.js';
 import {notifySuccess} from "../utils.js";
 
 export default class TplCurrentTask extends Tpl {
+    #tplNoTasks = null;
+    #tplHaveTasks = null;
+    #tplCreateTask = null;
+    
+    #tasks = [];
+    
     static get htmlPath() {
         return 'current-task';
     }
@@ -12,69 +18,98 @@ export default class TplCurrentTask extends Tpl {
     }
     
     async init(userId) {
-        const tasks = await Task.allForUser(userId);
+        this.#tasks = await Task.allForUser(userId);
+        this.#fillTaskList();
         
-        const tplNoTasks = this.getElement().querySelector('.no-tasks');
-        const tplHaveTasks = this.getElement().querySelector('.have-tasks');
-        const tplCreateTask = this.getElement().querySelector('.edit-task');
+        this.#tplNoTasks = this.getElement().querySelector('.no-tasks');
+        this.#tplHaveTasks = this.getElement().querySelector('.have-tasks');
+        this.#tplCreateTask = this.getElement().querySelector('.edit-task');
 
-        if (tasks.length === 0) {
-            tplNoTasks.hidden = false;
+        if (this.#tasks.length === 0) {
+            this.#tplNoTasks.hidden = false;
         } else {
-            tplHaveTasks.hidden = false;
+            this.#tplHaveTasks.hidden = false;
         }
-
-        tplNoTasks.querySelector('.btn-create-task').addEventListener('click', () => {
-            tplNoTasks.hidden = true;
-            tplHaveTasks.hidden = true;
-            tplCreateTask.hidden = false;
-            tplCreateTask.querySelector('[name="name"]').focus();
-        });
         
-        tplCreateTask.addEventListener('click', (event) => {
+        this.#listenBtnCreateTaskClick();
+        this.#listenTaskNameInput();
+        this.#listenColorSampleClick();
+        this.#listenCreateTaskSubmit(userId);
+        this.#listenCreateTaskCancel();
+    }
+    
+    #listenBtnCreateTaskClick() {
+        this.#tplNoTasks.querySelector('.btn-create-task').addEventListener('click', () => {
+            this.#tplNoTasks.hidden = true;
+            this.#tplHaveTasks.hidden = true;
+            this.#tplCreateTask.hidden = false;
+            this.#tplCreateTask.querySelector('[name="name"]').focus();
+        });
+    }
+    
+    #listenTaskNameInput() {
+        this.#tplCreateTask.querySelector('[name="name"]').addEventListener('input', (event) => {
+            event.target.classList.remove('invalid');
+            event.target.nextElementSibling.innerText = '';
+        });
+    }
+    
+    #listenColorSampleClick() {
+        this.#tplCreateTask.addEventListener('click', (event) => {
             if (!event.target.closest('.color-sample')) return;
-            
-            tplCreateTask.querySelector('[name="color"]').value = event.target.style.backgroundColor;
-            
-            for (const colorSample of tplCreateTask.querySelectorAll('.color-sample')) {
+
+            this.#tplCreateTask.querySelector('[name="color"]').value = event.target.style.backgroundColor;
+
+            for (const colorSample of this.#tplCreateTask.querySelectorAll('.color-sample')) {
                 colorSample.classList.remove('selected');
             }
             event.target.classList.add('selected');
         });
-
-        tplCreateTask.querySelector('[name="name"]').addEventListener('input', (event) => {
-            event.target.classList.remove('invalid');
-            event.target.nextElementSibling.innerText = '';
-        });
-        
-        tplCreateTask.querySelector('.btn-create-task-submit').addEventListener('click', async () => {
-            if (!tplCreateTask.querySelector('[name="name"]').value) {
-                tplCreateTask.querySelector('[name="name"]').classList.add('invalid');
-                tplCreateTask.querySelector('[name="name"]').nextElementSibling.innerText = "У задачі повинно бути ім'я.";
+    }
+    
+    #listenCreateTaskSubmit(userId) {
+        this.#tplCreateTask.querySelector('.btn-create-task-submit').addEventListener('click', async () => {
+            if (!this.#tplCreateTask.querySelector('[name="name"]').value) {
+                this.#tplCreateTask.querySelector('[name="name"]').classList.add('invalid');
+                this.#tplCreateTask.querySelector('[name="name"]').nextElementSibling.innerText = "У задачі повинно бути ім'я.";
                 return;
             }
-            
+
             const task = new Task();
             task.userId = document.querySelector('header .profile').dataset.userId;
-            task.taskName = tplCreateTask.querySelector('[name="name"]').value;
-            task.description = tplCreateTask.querySelector('[name="description"]').value;
-            task.color = tplCreateTask.querySelector('[name="color"]').value;
-            
+            task.taskName = this.#tplCreateTask.querySelector('[name="name"]').value;
+            task.description = this.#tplCreateTask.querySelector('[name="description"]').value;
+            task.color = this.#tplCreateTask.querySelector('[name="color"]').value;
+
             await task.save();
-            
+
             notifySuccess('BeeTrack', `Задача "${task.taskName}" успішно створена!"`);
-            tplCreateTask.hidden = true;
-            tplHaveTasks.hidden = false;
+            this.#tplCreateTask.hidden = true;
+            this.#tplHaveTasks.hidden = false;
+            this.#tasks = await Task.allForUser(userId);
+            this.#fillTaskList();
         });
+    }
+    
+    #listenCreateTaskCancel() {
+        this.#tplCreateTask.querySelector('.btn-create-task-cancel').addEventListener('click', () => {
+            this.#tplCreateTask.hidden = true;
 
-        tplCreateTask.querySelector('.btn-create-task-cancel').addEventListener('click', () => {
-            tplCreateTask.hidden = true;
-
-            if (tasks.length === 0) {
-                tplNoTasks.hidden = false;
+            if (this.#tasks.length === 0) {
+                this.#tplNoTasks.hidden = false;
             } else {
-                tplHaveTasks.hidden = false;
+                this.#tplHaveTasks.hidden = false;
             }
         });
+    }
+    
+    #fillTaskList() {
+        const selectEl = document.getElementById('tasklist');
+        
+        for (const task of this.#tasks) {
+            const optionEl = new Option(task.name, task.id.toString(), false, false);
+            optionEl.dataset.color = task.color;
+            selectEl.add(optionEl);
+        }
     }
 }
